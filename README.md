5dd90ebc2f74435c97bb0ec460eb92a03450f3c4
Petter Norgren <pn@skarvtech.com>
2020-05-20 09:01:32 +0200

    Added docker image.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# uwrosys
-UWROSYS ITP of Horizon 2020 RobMoSys project
+# `uwrosys_docker`: Docker image for the UWROSYS project
+
+This repository contains the source code for running Skarv Technologies' UWROSYS project based on UUVSimulator. UWROSYS is an ITP supported through RobMoSys - a Horizon 2020 project.
+
+# Installation
+
+**Install docker:**
+```
+sudo apt-get install docker.io
+```
+
+**Build docker image:**
+```
+cd <docker_directory>
+make build
+```
+
+**Run SmartMDSDD toolchain on local computer**
+```
+cd <docker_directory>
+make toolchain
+```
+
+**Connect additional terminal to image (only works when container is running)**
+```
+cd <docker_directory>
+make shell
+```
+
+**Clean stopped docker containers**
+```
+cd <docker_directory>
+make clean
+```
+
+**Clean docker system (requires full rebuild of all images)**
+```
+cd <docker_directory>
+make clean_system
+```
---
+++ docker/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright (c) 2020 Skarv Technologies AS
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Project: UWROSYS
+#          RobMoSys, Horizon 2020
+#
+# Author: Petter Norgren
+#
+
+# Source environmental variables
+source ~/.bashrc
+source ~/.profile
+
+sudo /etc/init.d/dbus start
+dbus-daemon --session --fork
+
+# Launch Eclipse
+$TOOLCHAIN_DIR/eclipse
+
+# Exit
+exit 0
