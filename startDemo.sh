b7e19f6e790845216e86d6bc45b33bf23454d99e
uwrosys <contact@skarvtech.com>
2020-11-20 14:22:21 +0100

    Updated startDemo-script.

--- startDemo.sh
+++ startDemo.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
 smartsoft_dir=$(dirname "$0")/repos/SystemUwrosysDemo/smartsoft
-cd smartsoft_dir
-bash ./src-gen/deployment/deploy-all.sh
-bash ./src-gen/deployment/start-all.sh
+cd $smartsoft_dir
+#chmod +rx ./src-gen/deployment/combine_ini_files.sh
+#chmod +rx ./src-gen/deployment/deploy-PC1.sh
+chmod +x ./src-gen/deployment/start-all.sh
+bash ./src-gen/deployment/combine_ini_files.sh
+bash ./src-gen/deployment/deploy-PC1.sh
+#bash -c './src-gen/deployment/start-all.sh menu-start'
+xterm -title "Global Scenario Control" -e "cd src-gen/deployment; pwd; bash start-all.sh menu;echo;echo;echo;echo 'You can close this window now.'; echo; echo;bash --login"
 exit 0
